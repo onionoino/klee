4ab692e61ae5faca9b98f42bffed6dcfec060e2d
Graham Williams <Graham.Williams@togaware.com>
2023-09-08 15:49:32 +1000

    Remove gjw timestamp.

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,6 @@
 /// License: GNU General Public License, Version 3 (the "License")
 /// https://www.gnu.org/licenses/gpl-3.0.en.html
 //
-// Time-stamp: <Tuesday 2023-09-05 09:47:24 +1000 Graham Williams>
-//
 // This program is free software: you can redistribute it and/or modify it under
 // the terms of the GNU General Public License as published by the Free Software
 // Foundation, either version 3 of the License, or (at your option) any later
